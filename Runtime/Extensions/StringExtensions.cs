5c991a4be3c5acbc913425e003631e8c55984029
Rauno Villberg <rauno.villberg@gmail.com>
2021-03-18 18:49:09 +0200

    Add string.StartingWith and string.EndingWith extensions

--- Runtime/Extensions/StringExtensions.cs
+++ Runtime/Extensions/StringExtensions.cs
@@ -8,5 +8,21 @@ namespace MWG
         {
             return text.IndexOf(partialTextToLookFor, StringComparison.OrdinalIgnoreCase) != -1;
         }
+        
+        /// Returns the String, prepending "start" - if needed.
+        /// For example: StartingWith("line", "lia") returns "lialine"
+        /// But StartingWith("line", "lin") returns "line"
+        public static string StartingWith(this string line, string start)
+        {
+            return line.StartsWith(start) ? line : start + line;
+        }
+
+        /// Returns the String, appending "end" - if needed.
+        /// For example: EndingWith("line", "lia") returns "linelia"
+        /// But StartingWith("line", "ne") returns "line"
+        public static string EndingWith(this string line, string end)
+        {
+            return line.EndsWith(end) ? line : line + end;
+        }
     }
 }
