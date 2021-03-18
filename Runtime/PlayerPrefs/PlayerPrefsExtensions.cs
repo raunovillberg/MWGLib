0f8bda5b5088118795fcee4da9e7e36e95dade1e
Rauno Villberg <rauno.villberg@gmail.com>
2021-03-18 18:49:42 +0200

    Add PlayerPrefs.SetBool & PlayerPrefs.GetBool

---
+++ Runtime/PlayerPrefs/PlayerPrefsExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace MWG
+{
+    public static class PlayerPrefsX
+    {
+        public static void SetBool(string name, bool booleanValue)
+        {
+            UnityEngine.PlayerPrefs.SetInt(name, booleanValue ? 1 : 0);
+        }
+
+        public static bool GetBool(string name, bool defaultValue)
+        {
+            return UnityEngine.PlayerPrefs.HasKey(name)
+                ? UnityEngine.PlayerPrefs.GetInt(name) == 1
+                : defaultValue;
+        }
+    }
+}
